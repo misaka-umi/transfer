b60ef236473f7c953edd27ce54d1169a2a07215c
umiz <55798143+misaka-umi@users.noreply.github.com>
2022-06-12 16:06:21 +0800

    Update b.c

--- b.c
+++ b.c
@@ -1,23 +1,48 @@
 
-include <unistd.h>
-#include <stdio.h>#include <stdlib.h>#include <semaphore.h>#include <sys /mman.h>int* count=;
-sem_t*mutex1=NULL;sem_t *mutex2=NULL;int main( )
-{
-int i;
-mutexl=(sem_t* )mmap(NULL,sizeof(sem_t),PROT_READ|PROT_WRITE,MAP_ANON|MAP_SHARED,-1,0 ) ;
-mutex2=(sem_t* )mmap(NULL,sizeof(sem_t),PROT_READ|PROT_WRITE,MAP_ANONIMAP_SHARED,-1,0);
-count=(int* )mmap(NULL,sizeof(int), PROT_READ|PROT_WRITE,MAP_ANON|NAP_SHARED,-1,0)
-sem_init(mutex1,1,1);
-sem_init(mutex2,1,0);*count=0 ;
-if(fork()==o)i
-for(i=0;i<10; i++){
-printf ( "add continue!\n" );sem_wait(mutex1) ;
-*count+=5;
-sem_post(mutex2);}
-printf ( "add finish!n" ) ;exit(0);
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <semaphore.h>
+#include <pthread.h>
+int count=5;
+sem_t ml;
+sem_t m2;
+pthread_mutex_t mutex;
+void *thread1(void *arg){
+int i=0;
+for(i=0 ; i<10;i++){
+sem_wait(&m1);
+count+=5;
+sem_post(&m2);}
 }
-for(i=0;i<10 ; i++)
-printf ( "printf_continue! \n" );sem_wait(mutex2);
-printf ( "count:%din" ,*count);sem_post(mutex1);
-sem_destroy ( mutex1);sem_destroy ( mutex2);
-munmap(mutex1,sizeof(sem_t) ) ;munmap(mutex2,sizeof(sem_t) );munmap( count,sizeof(int) );printf( "finish all!\n" );return 0;
+void *thread2 (void *arg){
+int i=0;
+for( i=0 ; i<10;i++){
+sem_wait(&m2);
+printf( "count=%d\n", count);
+sem_post(&m1);
+}
+}
+int main()
+{
+pthread_t thr1;
+pthread_t thr2 ;
+int ret;
+ret=sem_init(&m1,0,1);
+if ( ret!=0)
+{
+printf ( "sem_init error ! " );
+exit(0) ;
+}
+ret=sem_init(&m2,0,0 ) ;
+if(ret!=0)
+{
+printf ( "sem_init error! " );
+exit(0 ) ;
+}
+pthread_create(&thr1,NULL,thread1,NULL);
+pthread_create(&thr2,NULL,thread2,NULL);
+pthread_join(thr1,NULL);
+pthread_join(thr2,NULL);
+exit(0);
+}                  
