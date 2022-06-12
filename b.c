fbf0b3c30fa6530b590cf2aafd877819fe2baf17
umiz <55798143+misaka-umi@users.noreply.github.com>
2022-06-12 15:47:50 +0800

    Update b.c

--- b.c
+++ b.c
@@ -1,11 +1,18 @@
-void main (void)
-{    int   x=5;
-if( fork( ) )
-  {
-x+=30;
-        printf (“%d\n”,x);
-}
- else 
-      printf(“%d\n”,x);
- printf((“%d\n”,x);
+
+include <unistd.h>
+#include <stdio.h>#include <stdlib.h>#include <semaphore.h>#include <sys /mman.h>int* count=;
+sem_t*mutex1=NULL;sem_t *mutex2=NULL;int main( )
+{
+int i;
+mutexl=(sem_t* )mmap(NULL,sizeof(sem_t),PROT_READ|PROT_WRITE,MAP_ANON|MAP_SHARED,-1,0 ) ;
+mutex2=(sem_t* )mmap(NULL,sizeof(sem_t),PROT_READ|PROT_WRITE,MAP_ANONIMAP_SHARED,-1,0);
+count=(int* )mmap(NULL,sizeof(int), PROT_READ|PROT_WRITE,MAP_ANON|NAP_SHARED,-1,0)
+sem_init(mutex1,1,1);
+sem_init(mutex2,1,0);*count=0 ;
+if(fork()==o)i
+for(i=0;i<10; i++){
+printf ( "add continue!\n" );sem_wait(mutex1) ;
+*count+=5;
+sem_post(mutex2);}
+printf ( "add finish!n" ) ;exit(0);
 }
